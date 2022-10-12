ab7faaee879b058ea7cf53cd5c059db3c2a6c7cb
seokgyuHong <hsg8974@naver.com>
2022-10-12 23:16:23 +0900

    서치바, 이미지 표시 컴포즈 UI 추가

--- app/src/main/java/com/example/searchimagecoroutine/MainActivity.kt
+++ app/src/main/java/com/example/searchimagecoroutine/MainActivity.kt
@@ -2,10 +2,73 @@ package com.example.searchimagecoroutine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import com.skydoves.landscapist.glide.GlideImage
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val viewModel: SearchImageViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Column {
+                SearchAppBar()
+                ImageView()
+            }
+        }
+    }
+
+    @Composable
+    private fun SearchAppBar() {
+        var query = remember { mutableStateOf(TextFieldValue("")) }
+
+        TextField(
+            value = query.value,
+            onValueChange = { onQueryChanged ->
+                query.value = onQueryChanged
+            },
+            leadingIcon = {
+                IconButton(onClick = {
+                    viewModel.fetchSearchImage(query.value.text)
+                }) {
+                    Icon(
+                        imageVector = Icons.Rounded.Search,
+                        tint = MaterialTheme.colors.onBackground,
+                        contentDescription = "Search Icon"
+                    )
+                }
+            },
+            maxLines = 1,
+            colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.Transparent),
+            textStyle = MaterialTheme.typography.subtitle1,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = MaterialTheme.colors.background, shape = RectangleShape)
+        )
+    }
+
+    @Composable
+    fun ImageView() {
+        GlideImage(imageModel = viewModel.firstImageItem.observeAsState().value?.link)
     }
 }
